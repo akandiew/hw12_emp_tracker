4ed5fbaa8c0c62d7ef8320e061a4a407977756fb
akandiew <andrewkandiew@Andrews-MBP.home>
2020-05-02 23:46:21 -0400

    installed handlers / updated mysql / copied similar code

--- schema.sql
+++ schema.sql
@@ -8,21 +8,26 @@ CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
-  role_id INT UNSIGNED NOT NULL,
-  manager_id INT UNSIGNED NOT NULL
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
+  
 );
 
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NULL,
   salary DECIMAL(10,0) NULL,
-  department_id INT UNSIGNED NOT NULL
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+
 );
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  dept_name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) NULL
+
 );
 
 SELECT * FROM employees
---
+++ seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('John', 'Smith', 100, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Mike', 'Jones', 200, 2);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Bob', 'Sullivan', 200, 2);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Kevin', 'OConnor', 300, 3);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Tom', 'Allan', 400, 4);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Burns', 250, 2);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Ruby', 'Goldman', 200, 2);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Wendy', 'Springer', 225, 2);
+
+
+
+
+
+INSERT INTO roles (animal_breed, animal_name, price, buyer_id) VALUES ('wolf', 'Cassie', 195, 1);
+INSERT INTO roles (animal_breed, animal_name, price, buyer_id) VALUES ('bear', 'Rachel', 745, 1);
+INSERT INTO roles (animal_breed, animal_name, price, buyer_id) VALUES ('hawk', 'Tobias', 850, 2);
+INSERT INTO roles (animal_breed, animal_name, price, buyer_id) VALUES ('gorilla', 'Marco', 400, 2);
+INSERT INTO roles (animal_breed, animal_name, price, buyer_id) VALUES ('tiger', 'Jake', 300, 2);
+
+INSERT INTO departments (animal_breed, animal_name, price, buyer_id) VALUES ('tiger', 'Jake', 300, 2);
